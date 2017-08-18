828f382f7d0ace32468264f748d40515c4722ffa
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-08-18 13:48:05 +0200

    update resources

--- src/database/seeds/CmsPackageTableSeeder.php
+++ src/database/seeds/CmsPackageTableSeeder.php
@@ -8,7 +8,7 @@ class CmsPackageTableSeeder extends Seeder
     public function run()
     {
         Package::insert([
-            ['id' => 13, 'name' => 'CMS Package', 'root' => 'cms', 'sort' => 13, 'active' => true]
+            ['id' => 200, 'name' => 'CMS Package', 'root' => 'cms', 'sort' => 20, 'active' => true]
         ]);
     }
 }
